223194c8d242fa34f29cbf0112b48195440dab85
vincent <vincent.arteaga@gmail.com>
2021-07-21 20:47:20 -0700

    lab complete

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(str)
+# reverses all the words in a string without reversing the order of the words (FAILED - 1)
+str.split(' ').collect { |el| el.reverse }.join(' ')
+# reverses all the words in another string without reversing the order of the words (FAILED - 2)
+
+# uses collect (FAILED - 3)
+end 
+
